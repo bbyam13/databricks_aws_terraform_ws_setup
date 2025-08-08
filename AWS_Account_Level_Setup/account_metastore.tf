233e5cd33c82fd01587b16ead076a36db0fe9a52
bbyam13 <brendanbyam@gmail.com>
2025-08-08 13:58:59 -0400

    use var for specify region of metastore

--- AWS_Account_Level_Setup/account_metastore.tf
+++ AWS_Account_Level_Setup/account_metastore.tf
@@ -1,8 +1,8 @@
 resource "databricks_metastore" "this" {
   provider = databricks.accounts
   name          = "wldemo-metastore"
-  owner         = var.user_name
-  region        = "us-east-1"
+  owner         = var.user_name #this should be the user/service principal executing the terraform
+  region        = var.region
   force_destroy = true
 }
 
@@ -10,5 +10,10 @@ resource "databricks_metastore" "this" {
 resource "time_sleep" "wait_for_metastore" {
   depends_on = [databricks_metastore.this]
   create_duration = "30s"
+}
+
+output "metastore_id" {
+  value = databricks_metastore.this.id
+  description = "Unity Catalog metastore ID"
 }
 
